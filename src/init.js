93f459d86f221c510298267289158b4614618e4c
Dabiny <lina917@naver.com>
2023-03-26 22:19:04 +0900

    PORT env

--- src/init.js
+++ src/init.js
@@ -13,4 +13,4 @@ const PORT = process.env.PORT || 4000;
 
 // listen의 callback함수는 서버가시작될때 작동하는 함수다.
 const handleListening = () => console.log("Server listening on port 4000");
-app.listen(POR, handleListening);
+app.listen(PORT, handleListening);
